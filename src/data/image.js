052607e67986c7ea3a06966a25832732134ecee1
Shumaila <sayedshumaila27@gmail.com>
2025-03-20 00:40:46 +0530

    Add assets

---
+++ src/data/image.js
@@ -0,0 +1,46 @@
+const images = [
+	{
+		imgUrl: './gojo.jpg',
+		imgAlt: '',
+		hasClicked: false,
+		name: 'Gojo',
+	},
+
+	{
+		imgUrl: './kakashi.jpg',
+		imgAlt: '',
+		hasClicked: false,
+		name: 'Kakashi',
+	},
+
+	{
+		imgUrl: './midoriya.jpg',
+		imgAlt: '',
+		hasClicked: false,
+		name: 'Midoriya',
+    },
+    
+	{
+		imgUrl: './naruto.jpg',
+		imgAlt: '',
+		hasClicked: false,
+		name: 'Naruto',
+    },
+    
+	{
+		imgUrl: './shinobu.jpg',
+		imgAlt: '',
+		hasClicked: false,
+		name: 'Shinobu',
+    },
+    
+	{
+		imgUrl: './yor.jpg',
+		imgAlt: '',
+		hasClicked: false,
+		name: 'Yor',
+	},
+
+];
+
+export default images
